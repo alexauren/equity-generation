6e1c9637d82d123f7a449c6ebd31d4ba0c33ff7e
alexauren <alexander.auren@gmail.com>
2024-11-20 16:51:02 +0900

    Split the series of the SPY price to represent five years.

--- data/fetch.py
+++ data/fetch.py
@@ -9,9 +9,21 @@ class FETCH():
         self.write(data, stock)
     
     @staticmethod
-    def write(data, filename: str) -> None:
+    def write(data:tuple, filename: str) -> None:
         df = pd.DataFrame(data)
-        df.to_csv(f"data/csv/{filename}")
+        df.to_csv(f"data/csv/{filename}.csv")
+        
+    @staticmethod
+    def split(data: str) -> None:
+        df = pd.read_csv(f"data/csv/{data}")
+        close = df['4. close'].values
+        num_series = len(close) // (252 * 5)
+        
+        for i in range(num_series):
+            start_idx = i * 252 * 5
+            end_idx = min(start_idx + (252 * 5), len(close))
+            series = close[start_idx:end_idx]
+            pd.Series(series).to_csv(f"data/csv/spy_series/{i+1}.csv")
 
 FETCH().fetch("1MKFCLOIJI3QPW77", "SPY", 'full')
-    
+FETCH().split("SPY.csv")
